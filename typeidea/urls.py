a9b22a3ac636dea9e326c63ba966e570116567e9
cxq <1986541495@qq.com>
2019-04-19 21:17:07 +0800

    before common

--- typeidea/urls.py
+++ typeidea/urls.py
@@ -20,14 +20,18 @@ from .custom_site import custom_site
 from blog.views import post_list,post_detail
 from blog.views import (
     IndexView,CategoryView,TagView,
-    PostDetailView,
+    PostDetailView,SearchView,AuthorView
 )
+from config.views import LinkListView
 
 urlpatterns = [
     url(r'^$',IndexView.as_view(),name='index'),
     url(r'^category/(?P<category_id>\d+)/$',CategoryView.as_view(),name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$',TagView.as_view(),name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html/$',PostDetailView.as_view(),name='post-detail'),
+    url(r'^search/$',SearchView.as_view(),name='search'),
+    url(r'^author/(?P<owner_id>\d+)/$',AuthorView.as_view(),name='author'),
     url(r'^super_admin/',admin.site.urls),
     url(r'^admin/', custom_site.urls),
+    url(r'^links/$',LinkListView.as_view(),name='links'),
 ]
